7362933f5f6c77232c2a237c19d574cad4897f78
xarlos <xarlos@command1.net.xarlos.me>
2015-12-17 11:28:58 +0000

    slight non-essential amendments in case of the syntax of perlisms

--- __module__.pm
+++ __module__.pm
@@ -38,7 +38,7 @@ task 'setup', sub {
         file "/etc/salt/minion.d/masterfinger.conf",
                	content => template("files/etc/minion.d/masterfinger.tmpl", conf => { master_finger => "$master_finger" }),
                	on_change => sub { 
-                       	say "master fingerprint added/updated. ";
+                       	say "master fingerprint added or updated. ";
                        	service "salt-minion => "restart";
 			}
 
